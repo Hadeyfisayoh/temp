93834dd3834784a5840a42aae23994560672e018
Enoch Adebisi <adebisienoch5@gmail.com>
2023-08-17 18:39:31 +0100

    first commit

---
+++ module.js
@@ -0,0 +1,31 @@
+const secret = 'SUPER SECRET'
+const john = 'john'
+const peter = 'peter'
+
+module.exports = { john, peter}
+
+
+// const amount = 9
+
+// // if (amount< 10) {
+// //     console.log('small number');
+// // }
+// // else {
+// //     console.log('large number');
+// // }
+
+// // console.log('My app ');
+
+
+// // modules - Encapsulated code (only share minimum)
+// const names = require('./module')
+// const sayHi =  require('./utils')
+// const data = require('./alternative')
+// require('./mind')
+
+// // sayHi('susan')
+// // sayHi(names.john)
+// // sayHi(names.peter)
+
+
+
